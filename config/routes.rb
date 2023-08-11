8cd6daac9389f6f014bfb9f023bcf66303cf95ce
Vitalii Shevchenko <vshevchenko12@gmail.com>
2023-08-11 14:16:56 -0300

    Changed root page to the list of friends and list of friends converted to the table; added delete buttons, and remove link friends from navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :friends
   # get 'home/index'
-  root to: 'home#index'
+  # root to: 'home#index'
+  root to: 'friends#index'
   # get 'about' => 'home#about'
   get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
