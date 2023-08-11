10d071880289d8fc03db6b45c84d4aefb66a64ec
Vitalii Shevchenko <vshevchenko12@gmail.com>
2023-08-10 23:34:26 -0300

    Associated the friends with the users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_10_021048) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_11_020526) do
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_10_021048) do
     t.string "twiter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
